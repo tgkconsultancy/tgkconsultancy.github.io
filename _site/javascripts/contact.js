14c8a1dd7d3526a8d6e0c736b5ec3e4b9f20a494
VenkatramanC <venkatraman.c9@gmail.com>
2016-02-23 14:48:16 +0530

    Final changes Done

--- _site/javascripts/contact.js
+++ _site/javascripts/contact.js
@@ -1,5 +1,8 @@
+$(document).ready(function(){
 var contactform =  document.getElementById('contactFrm');
    contactform.setAttribute('action', '//formspree.io/' + 'tgkconsultancy' + '@' + 'gmail' + '.' + 'com');
-
-
+  
+ document.getElementById('name').innerHTML = 'Mobile No : +919866685715';
+ document.getElementById('email').innerHTML = 'tgkconsultancy@gmail.com';
+ 
 });
